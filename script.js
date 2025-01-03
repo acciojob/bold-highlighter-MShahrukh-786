1fd3821bb8172bf15e0f483c257d33c6e5b3aa0a
MShahrukh-786 <mshahrukh0636@gmail.com>
2025-01-03 22:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function highlight() {
     //Write your code here
 
+// highlight() function
+function highlight() {
+  const boldWords = document.querySelectorAll('strong');
+  boldWords.forEach((word) => {
+    word.style.color = 'green';
+  });
+}
+
+// return_normal() function
+function return_normal() {
+  const boldWords = document.querySelectorAll('strong');
+  boldWords.forEach((word) => {
+    word.style.color = 'black';
+  });
+}
+
+// Add event listeners to the link
+const link = document.querySelector('a');
+link.addEventListener('mouseover', highlight);
+link.addEventListener('mouseout', return_normal);
+
+
 
 }
 
